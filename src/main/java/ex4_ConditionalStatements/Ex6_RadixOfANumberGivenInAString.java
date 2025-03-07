4b8a256cb121dc497ad3f3745a933b587fdef3e5
navinkumarsingh <navin.bmsce@gmail.com>
2025-03-07 23:56:31 +0530

    java first commit

---
+++ src/main/java/ex4_ConditionalStatements/Ex6_RadixOfANumberGivenInAString.java
@@ -0,0 +1,10 @@
+package ex4_ConditionalStatements;
+import java.util.Scanner;
+public class Ex6_RadixOfANumberGivenInAString {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number :"+sc.nextLine());
+
+
+    }
+}
