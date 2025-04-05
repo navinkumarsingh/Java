f5d5bb2b40cd024a523777112671460e90783075
navinkumarsingh <navin.bmsce@gmail.com>
2025-04-06 01:13:29 +0530

    String Ex reverse A String

--- src/main/java/strings/Ex2_ReverseAString.java
+++
@@ -1,5 +0,0 @@
-package strings;
-
-public class Ex2_ReverseAString {
-
-}
---
+++ src/main/java/strings/Ex3_reverseAStringApproach2.java
@@ -0,0 +1,19 @@
+package strings;
+
+import java.util.Scanner;
+//Approach2: By converting String to char array type
+public class Ex3_reverseAStringApproach2 {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter a String");
+        String str = sc.nextLine();
+        String strRev = "";
+        char a[] = str.toCharArray();
+        for(int i=a.length-1;i>=0;i--)
+        {
+            strRev=strRev+a[i];
+        }
+        System.out.println(strRev);
+
+    }
+}
