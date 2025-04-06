83d724bf1a70943c70e7356902b745421b20c673
navinkumarsingh <navin.bmsce@gmail.com>
2025-04-06 23:18:57 +0530

    String Programs

---
+++ src/main/java/strings/Ex6_StringVsStringBufferVsStringBuilder.java
@@ -0,0 +1,20 @@
+package strings;
+
+public class Ex6_StringVsStringBufferVsStringBuilder {
+    public static void main(String[] args) {
+        //String - immutable
+        String s ="welcome";
+        s.concat("to java");
+        System.out.println(s); //immutable, cannot change original value of s
+
+        //StringBuffer - mutable
+        StringBuffer sb1 = new StringBuffer("welcome");
+        sb1.append("to java");
+        System.out.println(sb1);
+
+        //StringBuilder - Immutable
+        StringBuilder sb2 = new StringBuilder("welcome");
+        sb2.append("to java");
+        System.out.println(sb2);
+    }
+}
