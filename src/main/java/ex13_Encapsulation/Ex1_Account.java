8c199eb2a459add14698fb2f7b85285270af5576
navinkumarsingh <navin.bmsce@gmail.com>
2025-04-14 05:08:02 +0530

    Encapsulation Concept

---
+++ src/main/java/ex13_Encapsulation/Ex1_Account.java
@@ -0,0 +1,37 @@
+package ex13_Encapsulation;
+
+public class Ex1_Account {
+    private int accNo;
+    private String name;
+    private double amount;
+
+
+    public void setAccNo(int accNo) {
+         /*If class and local variable will be same, method will confuse, and it will assign default value(i.e. 0). Hence
+        Zero/0 will be returned from acc.getAccNo in Ex1_AccountMain */
+        //accNo=accNo;
+
+        //So we use this keyword to differentiate class and local variable. this keyword represents class.
+        this.accNo = accNo;
+    }
+
+    public int getAccNo() {
+        return accNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
